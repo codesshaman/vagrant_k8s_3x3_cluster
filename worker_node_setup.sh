68b837478173243d8dcc7e7f9a201f2141edd46b
codesshaman <nicksemail@yandex.ru>
2023-05-16 09:03:50 +0300

    without kubespray

--- worker_node_setup.sh
+++ worker_node_setup.sh
@@ -50,9 +50,7 @@ apt-get install -y \
     make \
     tmux \
     gnupg \
-    haproxy \
     iptables \
-    keepalived \
     libnss3-tools
 echo -e "${warn}[k8s installer]${no} ${cyan}Установка mkcert для самоподписных сертификатов${no}"
 curl -s https://api.github.com/repos/FiloSottile/mkcert/releases/latest| grep browser_download_url  | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
